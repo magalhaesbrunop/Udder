ad6c4c992dbea872827f6dbe3af23e8b91b99a22
Bruno Magalhães <magalhaesbrunop@gmail.com>
2018-11-27 09:52:12 -0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Bruno Magalhães on 27/11/18.
+//  Copyright © 2018 Cybernetic Company of Milky Way. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
